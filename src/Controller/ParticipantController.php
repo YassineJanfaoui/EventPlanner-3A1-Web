64a41d0a28438ceda92c12bb9024475f423146be
RebhiAyoub <ayoub.rebhi@esprit.tn>
2025-04-16 00:31:11 +0100

    Participant front & back

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -14,10 +14,18 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/participant')]
 final class ParticipantController extends AbstractController
 {
-    #[Route(name: 'app_participant_index', methods: ['GET'])]
+    #[Route('/',name: 'app_participant_index', methods: ['GET'])]
     public function index(ParticipantRepository $participantRepository): Response
     {
         return $this->render('participant/index.html.twig', [
+            'participants' => $participantRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/back',name: 'app_participant_index_back', methods: ['GET'])]
+    public function display_back(ParticipantRepository $participantRepository): Response
+    {
+        return $this->render('participant/displayBack.html.twig', [
             'participants' => $participantRepository->findAll(),
         ]);
     }
@@ -42,10 +50,38 @@ final class ParticipantController extends AbstractController
         ]);
     }
 
+    #[Route('/newBack', name: 'app_participant_new_back', methods: ['GET', 'POST'])]
+    public function new_back(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $participant = new Participant();
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($participant);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_participant_index_back', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('participant/newBack.html.twig', [
+            'participant' => $participant,
+            'form' => $form,
+        ]);
+    }
+
     #[Route('/{participantId}', name: 'app_participant_show', methods: ['GET'])]
     public function show(Participant $participant): Response
     {
         return $this->render('participant/show.html.twig', [
+            'participant' => $participant,
+        ]);
+    }
+
+    #[Route('/back/{participantId}', name: 'app_participant_show_back', methods: ['GET'])]
+    public function showBack(Participant $participant): Response
+    {
+        return $this->render('participant/showBack.html.twig', [
             'participant' => $participant,
         ]);
     }
@@ -68,6 +104,26 @@ final class ParticipantController extends AbstractController
         ]);
     }
 
+    #[Route('/{participantId}/edit_back', name: 'app_participant_edit_back', methods: ['GET', 'POST'])]
+    public function editBack(Request $request, Participant $participant, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(ParticipantType::class, $participant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_participant_index_back', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('participant/editBack.html.twig', [
+            'participant' => $participant,
+            'form' => $form,
+        ]);
+    }
+
+
+
     #[Route('/{participantId}', name: 'app_participant_delete', methods: ['POST'])]
     public function delete(Request $request, Participant $participant, EntityManagerInterface $entityManager): Response
     {
@@ -76,6 +132,18 @@ final class ParticipantController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_participant_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_participant_index_back', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+    #[Route('/{participantId}', name: 'app_participant_delete_back', methods: ['POST'])]
+    public function deleteBack(Request $request, Participant $participant, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$participant->getParticipantId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($participant);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_participant_index_back', [], Response::HTTP_SEE_OTHER);
     }
 }
